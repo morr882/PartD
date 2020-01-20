09460df60f67fcfdd66ef52f0dd51429f6e0d701
morr882 <morr@royalroad.school.nz>
2020-01-21 11:30:02 +1300

    4th commit to program.

--- Part D/src/partD.java
+++ Part D/src/partD.java
@@ -6,5 +6,6 @@ public class partD {
 	System.out.println("Hi my name is Maria");
 	System.out.println("My favourite colour is purple");
 	System.out.println("My favourite mode of transportation is train");
+	System.out.println("The best day of the week is Friday");
 	}
 }
